5ce4576f75101e15ccafb8267f84b1fa7ae54871
Ertuğrul Gürler <108678551+ErtugrulGurler@users.noreply.github.com>
2022-10-06 23:01:59 +0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const colors = ['red','white'];
-let oyuncu1 = document.getElementById("player1");
-let oyuncu2 = document.getElementById("player2");
 let oyuncu3 = document.getElementById("player3");
 let oyuncu4 = document.getElementById("player4");
 let oyuncu5 = document.getElementById("player5");
@@ -11,38 +9,84 @@ let casus1 = document.getElementById("spy1");
 let casus2 = document.getElementById("spy2");
 let casus3 = document.getElementById("spy3");
 
-let oyuncuListe = [oyuncu1,oyuncu2,oyuncu3,oyuncu4,oyuncu5,oyuncu6,oyuncu7,oyuncu8];
+let oyuncuListe = [oyuncu3,oyuncu4,oyuncu5,oyuncu6,oyuncu7,oyuncu8];
 let casusListe = [casus1,casus2,casus3];
 function paragrafEkle(){
     
 }
 function oyuncuSayısı(){
-    for (let i=0;i<=7;i++){
+    /*for (let i=0;i<=7;i++){
         let oyuncu = oyuncuListe[i];
         if(oyuncu.style.backgroundColor==="red"){
             return i+1;
         }
-    }
+    }*/
 }
 function casusSayısı(){
-    for (let i=0;i<=2;i++){
+    /*for (let i=0;i<=2;i++){
         let casus = casusListe[i];
         if(casus.style.backgroundColor==="red"){
             return i+1;
         }
-    }
+    }*/
 }
-for (let i=0;i<=7;i++){
-    let index = 0;
+for (let i=0;i<=5;i++){
+    let flag=false;
+
     oyuncuListe[i].addEventListener('click', function onClick() {
-    oyuncuListe[i].style.backgroundColor = colors[index];
-    
-    index = index >= colors.length - 1 ? 0 : index + 1;
+    if(flag===false){
+    oyuncuListe[i].style.backgroundColor = "red";
+    for(let x=0;x<=5;x++){
+        if(oyuncuListe[i]!==oyuncuListe[x]){
+            oyuncuListe[x].disabled = true;
+            flag =true;
+        }
+        }
+    }
+    else {
+        for(let x=0;x<=5;x++){
+                oyuncuListe[x].disabled = false;
+                flag =false;
+                oyuncuListe[i].style.backgroundColor = "bisque";
+            }
+    }
 });}
 for (let i=0;i<=2;i++){
-    let index = 0;
+    let flag=false;
+
     casusListe[i].addEventListener('click', function onClick() {
-    casusListe[i].style.backgroundColor = colors[index];
-    
-    index = index >= colors.length - 1 ? 0 : index + 1;
+    if(flag===false){
+    casusListe[i].style.backgroundColor = "red";
+    for(let x=0;x<=2;x++){
+        if(casusListe[i]!==casusListe[x]){
+            casusListe[x].disabled = true;
+            flag =true;
+        }
+        }
+    }
+    else {
+        for(let x=0;x<=2;x++){
+                casusListe[x].disabled = false;
+                flag =false;
+                casusListe[i].style.backgroundColor = "bisque";
+            }
+    }
 });}
+
+function oyuncuCasusSec(){
+    for(let i=0;i<=5;i++){
+        let flag1=false;
+        if(oyuncuListe[i].style.backgroundColor==="red"){
+            flag1=true;
+        }
+    }
+    for(let i=0;i<=2;i++){
+        let flag2=false;
+        if(casusListe[i].style.backgroundColor==="red"){
+            flag2=true;
+        }
+    }
+    if(flag1===false||flag2===false){
+        alert("Oyuncu-casus seçilmedi.")
+    }
+}
